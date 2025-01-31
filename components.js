4e2a87f3286d55d30585464cebf8171d01b9b50c
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-01-31 11:35:53 -0600

    add json array for each project

---
+++ components.js
@@ -0,0 +1,170 @@
+const projects = [
+    {
+        id:'project18',
+        title: 'To Do blog application in React',
+        description: 'This is a simple blog code project made in React JS',
+        image:'/assets/code/react-appThumbnail.jpg',
+        modalId:'',
+        tags:['code'],
+        link:''
+    },
+    {
+        id:'project16',
+        title: 'Drawing App in Java',
+        description: 'This is a simple drawing app made in Java code',
+        image:'/assets/code/javadrawingapp.jpg',
+        modalId:'',
+        tags:['code'],
+        link:'https://github.com/ViLinh27/se450-FinalProject'
+    },
+    {
+        id:'project7',
+        title: 'Time To Talk',
+        description: 'This is a visual novel game project',
+        image:'/assets/timeToTalk-itchioCoverArtboard-1.png',
+        modalId:'',
+        tags:['code'],
+        link:'https://linh-is-tibia.itch.io/time-to-talk'
+    },
+    {
+        id:'project8',
+        title: 'Cartridge Collector',
+        description: 'This is a platformer game project',
+        image:'/assets/code/react-appThumbnail.jpg',
+        modalId:'',
+        tags:['code'],
+        link:'https://linh-is-tibia.itch.io/cartridge-collector'
+    },
+    {
+        id:'project9',
+        title: 'Hell Ghost',
+        description: 'This is an endless runner game.',
+        image:'/assets/ghost01.JPG',
+        modalId:'',
+        tags:['code'],
+        link:'https://linh-is-tibia.itch.io/hellghost'
+    },
+    {
+        id:'project5',
+        title: 'SQL',
+        description: 'I explore Sql here',
+        image:'https://optim.tildacdn.one/tild6238-3035-4335-a333-306335373139/-/resize/760x/-/format/webp/IMG_3349.jpg',
+        modalId:'myModal5',
+        tags:['code'],
+        link:''
+    },
+    {
+        id:'project10',
+        title: 'Android Notes',
+        description: 'A simple android app from my Android development class',
+        image:'/assets/code/android-code.jpg',
+        modalId:'myModal10',
+        tags:['code'],
+        link:''
+    },
+    {
+        id:'project11',
+        title: 'IOS book tracker',
+        description: 'A simple book tracker app from my IOS development class',
+        image:'https://images.pexels.com/photos/1294886/pexels-photo-1294886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        modalId:'myModal11',
+        tags:['code'],
+        link:''
+    },
+    {
+        id:'project12',
+        title: 'Rouyn Noranda UX project',
+        description: 'An exploration of the information architecture of an existing site.',
+        image:'https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        modalId:'myModal12',
+        tags:['ux'],
+        link:''
+    },
+    {
+        id:'project13',
+        title: 'UX discussion paper',
+        description: 'Discussing two external papers about UX and design.',
+        image:'https://images.pexels.com/photos/273230/pexels-photo-273230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        modalId:'myModal13',
+        tags:['ux'],
+        link:''
+    },
+    {
+        id:'project14',
+        title: 'PeezyFind',
+        description: 'A mobile application concept.',
+        image:'/assets/ux/peezyFindMoodboard.jpg',
+        modalId:'myModal14',
+        tags:['ux'],
+        link:''
+    },
+    {
+        id:'project1',
+        title: 'fan art',
+        description: 'This is the fan art gallery',
+        image:'https://pbs.twimg.com/media/FqjphO0WYAMtdu2?format=jpg&name=4096x4096',
+        modalId:'myModal1',
+        tags:['art'],
+        link:''
+    },
+    {
+        id:'project2',
+        title: 'Public Figure Portraits',
+        description: 'This is the portrait gallery of real people.',
+        image:'https://pbs.twimg.com/media/FiMjNK_WYAAWmls?format=jpg&name=medium',
+        modalId:'myModal2',
+        tags:['art'],
+        link:''
+    },
+    {
+        id:'project3',
+        title: 'Original Characters',
+        description: 'This is where my original characters are.',
+        image:'https://pbs.twimg.com/media/FhtpuF5XkAAynEm?format=jpg&name=medium',
+        modalId:'myModal3',
+        tags:['art'],
+        link:''
+    },
+    {
+        id:'project4',
+        title: 'DTIYS Challenges',
+        description: 'Instagram will often have drawing challenges. This is a gallery of them.',
+        image:'https://pbs.twimg.com/media/FX1X4vVVsAQnkPa?format=jpg&name=medium',
+        modalId:'myModal4',
+        tags:['art'],
+        link:''
+    },
+    {
+        id:'project15',
+        title: 'Concept Collaboration Project',
+        description: 'Collaborate on a film concept project',
+        image:'/assets/art/ella-collab/kitchen.jpg',
+        modalId:'myModal15',
+        tags:['art'],
+        link:''
+    },
+    {
+        id:'project17',
+        title: 'Layouts and Backgrounds',
+        description: 'Environments drawn in different styles.',
+        image:'/assets/art/layouts/layout5.jpg',
+        modalId:'myModal17',
+        tags:['art'],
+        link:''
+    },
+    /*{
+        id:'',
+        title: '',
+        description: '',
+        image:'',
+        modalId:'',
+        tags:[''],
+        link:''
+    }*/
+];
+export function createProjectGrid(){
+    return `
+    
+    `;
+
+}
