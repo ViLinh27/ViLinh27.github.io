147e1bd4a4908de0a853745e456e63b0942eeb5f
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-01-26 15:59:24 -0600

    get rid of scale to not conflict with parallax in css

--- filter.js
+++ filter.js
@@ -5,10 +5,12 @@ $("#portfolio-filters li").click ( function() {
     var selectedFilter = $(this).data("filter");
     $("#portfolio-wrapper").fadeTo(100, 0);
 
-    $(".pcontainer").fadeOut().css('transform', 'scale(0)');
+    //$(".pcontainer").fadeOut().css('transform', 'scale(0)');
+    $(".pcontainer").fadeOut().css('transform');
 
     setTimeout(function() {
-      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+      //$(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+      $(selectedFilter).fadeIn(100).css('transform');
       $("#portfolio-wrapper").fadeTo(300, 1);
     }, 300);
   });
