00d78d93185efd7ca48d6512b9e2ad6d0fc7d4ce
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-08-16 10:12:54 -0500

    responive banner text

--- components/header.js
+++ components/header.js
@@ -3,9 +3,14 @@ class Header extends HTMLElement {
     connectedCallback(){
         this.innerHTML = `
             <div class="banner">
+                <button onclick="toggleDarkMode()">toggle dark mode</button>
                 <img src="https://pbs.twimg.com/media/GKQQB2lW4AAwvFo?format=png&name=small" alt="dog" class="dog" />
                 <h1 class="banner-title">Vi-Linh Nguyen</h1>
                 <p class="banner-desc">Visual Development & Illustration | Software Development | UX Research</p>
+
+                <p class="banner-desc-responsive">Visual Development & Illustration |</p>
+                <p class="banner-desc-responsive">Software Development |</p>
+                <p class="banner-desc-responsive">UX Research</p>
             </div>
         `;
     }
