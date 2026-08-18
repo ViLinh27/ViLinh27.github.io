3c73e8ce33f3b515f28644b48c43ec2306ad391c
Vi-Linh Nguyen <vilinh27@gmail.com>
2026-08-18 09:24:44 -0500

    added misc pages and links
    need to fix links on main portfolio pages and see if it can lead to scroll position

--- components/navbar.js
+++ components/navbar.js
@@ -25,12 +25,12 @@ class Navbar extends HTMLElement{
                 <a class="dropbtn" href="#">Misc. &#8964;</a>
                 <div class="dropdown-content">
                     <a href="/pages/misc/writing.html">Writing</a>
-                    <a href="#">traditional art</a>
-                    <a href="#">sticker designs</a>
-                    <a href="#">zines</a>
-                    <a href="#">sculptures</a>
-                    <a href="#">fibre arts</a>
-                    <a href="#">Bookbinding</a>
+                    <a href="/pages/misc/traditional_art.html">traditional art</a>
+                    <a href="/pages/misc/sticker_designs.html">sticker designs</a>
+                    <a href="/pages/misc/zines.html">zines</a>
+                    <a href="/pages/misc/sculptures.html">sculptures</a>
+                    <a href="/pages/misc/fibre_arts.html">fibre arts</a>
+                    <a href="/pages/misc/book_binding.html">Bookbinding</a>
                 </div>
                 </div>
                 <a href="javascript:void(0);" class="icon" onclick="responsiveNavFunc()">
